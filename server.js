f28d2d1482a4424af8876b67036330f94280f5a6
John Ravan <john@jtravan.com>
2018-10-01 14:40:50 -0400

    Committing changes after Chapter 5

---
+++ server.js
@@ -0,0 +1,60 @@
+// import https from 'https';
+//
+// https.get('https://www.lynda.com', res => {
+//     console.log('Response status code: ', res.statusCode);
+//
+//     res.on('data', chunk => {
+//         console.log(chunk.toString());
+//     })
+// });
+
+// import http from 'http';
+// //
+// // const server = http.createServer();
+// //
+// // server.listen(8080);
+// //
+// // server.on('request', (req, res) => {
+// //     res.write('HELLO HTTP!\n');
+// //     setTimeout(() => {
+// //         res.write('I can stream!\n');
+// //         res.end();
+// //     }, 3000);
+// // });
+
+// server.get('/about.html', (req, res) => {
+//     fs.readFile('./about.html', (err, data) => {
+//         res.send(data.toString());
+//     });
+// });
+
+
+// import fs from 'fs';
+
+import config from './config';
+import apiRouter from './api';
+import sassMiddleware from 'node-sass-middleware';
+import path from 'path';
+
+import express from 'express';
+const server = express();
+
+server.use(sassMiddleware({
+    src: path.join(__dirname, 'sass'),
+    dest: path.join(__dirname, 'public')
+}));
+
+server.set('view engine', 'ejs');
+
+server.get('/', (req, res) => {
+    res.render('index', {
+        content: '...'
+    });
+});
+
+server.use('/api', apiRouter);
+server.use(express.static('public'));
+
+server.listen(config.port, () => {
+    console.info('Express listening on port ', config.port);
+});
---
+++ src/components/App.js
@@ -0,0 +1,36 @@
+import React from "react";
+import Header from './Header';
+import ContestPreview from './ContestPreview';
+import axios from 'axios';
+
+class App extends React.Component {
+    state = {
+        pageHeader: 'Naming Contests',
+        contests:[]
+    };
+
+    componentDidMount() {
+        axios.get('/api/contests')
+            .then(resp => {
+                this.setState({
+                   contests: resp.data.contests
+                });
+            })
+            .catch(console.err);
+    }
+
+    render () {
+        return (
+            <div className="App">
+                <Header message={this.state.pageHeader} />
+                <div>
+                    {this.state.contests.map(contest =>
+                        <ContestPreview key={contest.id} {...contest}/>
+                    )}
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
